02f9d62234487998fc65303fef0433f33f8c4f9b
Priyanshu_Tiwari <priyanshutiwari6789@gmail.com>
2022-06-05 16:50:57 +0530

    contact form functionality added

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <section id="contact" className="text-gray-400 bg-gray-900 body-font relative">
+    <section
+      id="contact"
+      className="text-gray-400 bg-gray-900 body-font relative"
+    >
       <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
@@ -16,30 +19,32 @@ const ContactUs = () => {
             scrolling="no"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227821.9337586808!2d80.80242359468362!3d26.848929332651974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd991f32b16b%3A0x93ccba8909978be7!2sLucknow%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1654353962277!5m2!1sen!2sin"
           ></iframe>
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md" >
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/3 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
-              <p className="mt-1">
-                Lucknow,Uttar Pradesh
-              </p>
+              <p className="mt-1">Lucknow,Uttar Pradesh</p>
             </div>
             <div className="lg:w-2/3 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-                priyanshutiwari6789@gmail.com
+              priyanshutiwari6789@gmail.com
             </div>
           </div>
         </div>
-        <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+
+        <form
+          action="https://formspree.io/f/mgerakwb"
+          method="POST"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-white text-4xl mb-1 font-medium title-font">
             &lt;Contact/&gt;
             <div className="h-1 w-1/4 bg-blue-500 rounded"></div>
-
           </h2>
-          
+
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
@@ -75,11 +80,13 @@ const ContactUs = () => {
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             ></textarea>
           </div>
-          <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
             Send Message
           </button>
-          
-        </div>
+        </form>
       </div>
     </section>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
 const Footer = () => {
   return (
     <footer className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+        <a
+          href="#contact"
+          className="flex title-font font-medium items-center md:justify-start justify-center text-white"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -21,8 +23,8 @@ const Footer = () => {
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
           © Copyright 2022
           <a
+            href="#contact"
             className="text-gray-500 ml-1"
-            target="_blank"
             rel="noopener noreferrer"
           >
             @Priyanshu Tiwari
