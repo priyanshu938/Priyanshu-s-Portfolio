a920716ad1c09885cbf53b8ff3af79092ba20e87
Priyanshu_Tiwari <priyanshutiwari6789@gmail.com>
2022-06-06 09:12:00 +0530

    project api added with real data

--- src/components/Projects/projectsAPI.js
+++ src/components/Projects/projectsAPI.js
@@ -1,52 +1,110 @@
 const projects = [
   {
     id: 1,
-    image: "https://dummyimage.com/200x200",
-    title: "Project 1",
-    description:
-      "skvnkvdknvdklvndkvndknvdkn dk ndknvkvnskvnkdvndkvndkvnknvdxkvndskvn sklnvskvnvnnvxklvndsn",
-    githubLink: "https://www.github.com",
-    liveProjectLink: "https://www.google.com",
-    youtubeVideoLink: "https://www.youtube.com",
-  },
-  {
-    id: 2,
-    image: "https://dummyimage.com/200x200",
-    title: "Project 2",
-    description:
-      "skvnkvdknvdklvndkvndknvdkn dk ndknvkvnskvnkdvndkvndkvnknvdxkvndskvn sklnvskvnvnnvxklvndsn",
-    githubLink: "https://www.github.com",
-    liveProjectLink: "https://www.google.com",
-    youtubeVideoLink: "https://www.youtube.com",
-  },
-  {
-    id: 3,
-    image: "https://dummyimage.com/200x200",
-    title: "Project 3",
-    description:
-      "skvnkvdknvdklvndkvndknvdkn dk ndknvkvnskvnkdvndkvndkvnknvdxkvndskvn sklnvskvnvnnvxklvndsn",
-    githubLink: "https://www.github.com",
-    liveProjectLink: "https://www.google.com",
-    youtubeVideoLink: "https://www.youtube.com",
-  },
-  {
-    id: 4,
-    image: "https://dummyimage.com/200x200",
-    title: "Project 4",
-    description:
-      "skvnkvdknvdklvndkvndknvdkn dk ndknvkvnskvnkdvndkvndkvnknvdxkvndskvn sklnvskvnvnnvxklvndsn",
-    githubLink: "https://www.github.com",
+    image: "images/tindog.png",
+    title: "Tindog Website",
+    description: "A simple static website made using HTML and CSS.",
+    githubLink: "https://github.com/priyanshu938/tindogSite",
     liveProjectLink: "",
     youtubeVideoLink: "",
   },
   {
-    id: 5,
-    image: "https://dummyimage.com/200x200",
-    title: "Project 5",
+    id: 2,
+    image: "images/blog.png",
+    title: "A Blog Website",
     description:
-      "skvnkvdknvdklvndkvndknvdkn dk ndknvkvnskvnkdvndkvndkvnknvdxkvndskvn sklnvskvnvnnvxklvndsn",
-    githubLink: "https://www.github.com",
-    liveProjectLink: "https://www.google.com",
+      "A blog website in which you can write your blogs built using EJS template engine and Node.js.",
+    githubLink: "https://github.com/priyanshu938/blogsitewithDB",
+    liveProjectLink: "https://blogsitewithdb.herokuapp.com/",
+    youtubeVideoLink: "",
+  },
+  {
+    id: 3,
+    image: "images/secrets.png",
+    title: "Project 3",
+    description:
+      "A Secrets share site on which users can visit and login thourgh their id's and share their secrets without their identity being disclosed.",
+    githubLink: "https://github.com/priyanshu938/Authentication_Secrets",
+    liveProjectLink: "",
+    youtubeVideoLink: "",
+  },
+  {
+    id: 4,
+    image: "images/todos.png",
+    title: "Todos App using React.js",
+    description:
+      "A todo list app which saves todos in your local storage in which you can write your daily taks and delete them when they are done.",
+    githubLink: "https://github.com/priyanshu938/todos-app-using-REACT",
+    liveProjectLink: "https://priyanshu938.github.io/todos-app-using-REACT/",
+    youtubeVideoLink: "",
+  },
+  {
+    id: 5,
+    image: "images/sachin-portfolio.png",
+    title: "Sachin's portfolio",
+    description: "Built a Portfolio site for my friend.",
+    githubLink: "https://github.com/priyanshu938/Sachin-Portfolio",
+    liveProjectLink: "https://sachin-portfolio-app.herokuapp.com/",
+    youtubeVideoLink: "https://youtu.be/HxpaU1p1_Cw",
+  },
+  {
+    id: 6,
+    image: "images/paging.png",
+    title: "Paging Algorithms Simulator",
+    description:
+      "A web app that simulates Paging Algorithms-FIFO,LFU,LRU and Optimal.",
+    githubLink: "https://github.com/priyanshu938/Paging-Algorithms-Simulator",
+    liveProjectLink: "https://online-paging-tool.herokuapp.com/",
+    youtubeVideoLink: "https://youtu.be/W1uZ9rn7rik",
+  },
+  {
+    id: 7,
+    image: "images/apitester.png",
+    title: "API Tester Project",
+    description:
+      "This is a simple Postman Clone made using HTML ,Bootstrap and vanilla Java script.",
+    githubLink: "https://github.com/priyanshu938/APITesterApp",
+    liveProjectLink: "https://priyanshu938.github.io/APITesterApp/",
+    youtubeVideoLink: "https://youtu.be/P2rh1bTx12I",
+  },
+  {
+    id: 8,
+    image: "images/starvis.jpg",
+    title: "Starvis AI Project",
+    description:
+      "This is a small demonstration of speech recognition module of python smartly made to perform various operations through speech recognition.",
+    githubLink: "https://github.com/priyanshu938/Starvis-AI-Project",
+    liveProjectLink: "",
+    youtubeVideoLink: "https://youtu.be/ULnO4bxj39c",
+  },
+  {
+    id: 9,
+    image: "images/notebook.jpg",
+    title: "My Notebook",
+    description:
+      "This app serves as your own notebook on cloud where you can login and store your notes.",
+    githubLink: "https://github.com/priyanshu938/MyNotebook",
+    liveProjectLink: " https://my-notebook-app-priyanshu983.netlify.app/",
+    youtubeVideoLink: "",
+  },
+  {
+    id: 10,
+    image: "images/movie.jpg",
+    title: "Movie App",
+    description:
+      "In this app you can search for a movie and it will fetch the details for that movie and will show you. ",
+    githubLink: "https://github.com/priyanshu938/Movie-App",
+    liveProjectLink: "",
+    youtubeVideoLink: "",
+  },
+  {
+    id: 11,
+    image: "images/compiler.png",
+    title: "Online Compiler",
+    description:
+      "Online Compiler Web App built using Html5,Node.js,Express.js and EJS templating engine. The web app supports copiling and executing programs in Java,C++,Python and C language respectively. You can also add console inputs. Press run button to see the output.",
+    githubLink: "https://github.com/priyanshu938/CompilerApp",
+    liveProjectLink: "https://your-own-compiler.herokuapp.com/",
     youtubeVideoLink: "",
   },
 ];
