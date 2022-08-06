457880aa81517284cbcf2f1b679323812c9ca06e
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-08-07 00:02:59 +0530

    +1

--- src/components/WorkExperiences/WorkExperiences.jsx
+++ src/components/WorkExperiences/WorkExperiences.jsx
@@ -16,7 +16,7 @@ const WorkExperiences = () => {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col items-center w-full mb-10">
           <h1 className="sm:text-4xl text-2xl font-medium title-font mb-4 text-white">
-            &lt;My Works /&gt;
+            &lt;My Work Experiences /&gt;
             <div className="h-1 w-3/4 bg-blue-500 rounded"></div>
           </h1>
         </div>
